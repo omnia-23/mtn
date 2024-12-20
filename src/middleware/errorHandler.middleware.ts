7d3c83a5079a7cc56d8b86f39b1823131536f778
omnia <omniakhaled239@gmail.com>
2024-12-20 12:19:07 +0200

    deploy

--- src/middleware/errorHandler.middleware.ts
+++ src/middleware/errorHandler.middleware.ts
@@ -2,7 +2,7 @@
 import { Request, Response, NextFunction } from 'express';
 import HttpError from '../utils/HttpError';
 
-export const errorHandler = (err: HttpError, req: Request, res: Response, _next: NextFunction) => {
+export const errorHandler = (err: ts, req: Request, res: Response, _next: NextFunction) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || 'Internal Server Error';
   res.status(statusCode).json({
