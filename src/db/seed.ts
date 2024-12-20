552b74aea1cd4e3b116e297bc3fe897bb91b2d2a
omnia <omniakhaled239@gmail.com>
2024-12-20 21:19:25 +0200

    feat : add seed command and updated seed database script

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,33 +1,48 @@
-// import bcrypt from 'bcrypt';
-// import db from './database';
-// import { roleTable, userRoleTable, userTable } from './models';
+import bcrypt from 'bcrypt';
+import db from './database';
+import { roleTable, userRoleTable, userTable } from './models';
 
-// const seedDatabase = async () => {
-//   try {
-//     const hashedPassword = await bcrypt.hash('password', 10);
-//     const [adminRole] = await db.insert(roleTable).values({ name: 'admin' }).returning();
+const seedDatabase = async () => {
+  try {
+    const hashedPassword = await bcrypt.hash('password', 10);
+    const [adminRole, customerRole] = await db
+      .insert(roleTable)
+      .values([{ name: 'admin' }, { name: 'customer' }])
+      .returning();
 
-//     const [adminUser] = await db
-//       .insert(userTable)
-//       .values({
-//         username: 'admin',
-//         email: 'admin@gmail.com',
-//         phone: '1234567890',
-//         password: hashedPassword,
-//       })
-//       .returning();
+    const [adminUser, customerUser] = await db
+      .insert(userTable)
+      .values([
+        {
+          username: 'admin',
+          email: 'admin@gmail.com',
+          password: hashedPassword,
+        },
+        {
+          username: 'customer',
+          email: 'customer@gmail.com',
+          password: hashedPassword,
+        },
+      ])
+      .returning();
 
-//     if (adminRole && adminUser) {
-//       await db.insert(userRoleTable).values({
-//         user_id: adminUser.id,
-//         role_id: adminRole.id,
-//       });
-//     }
+    if (adminRole && adminUser) {
+      await db.insert(userRoleTable).values({
+        user_id: adminUser.id,
+        role_id: adminRole.id,
+      });
+    }
+    if (customerRole && customerUser) {
+      await db.insert(userRoleTable).values({
+        user_id: customerUser.id,
+        role_id: customerRole.id,
+      });
+    }
 
-//     console.log('Seeding complete: Admin user with admin role added.');
-//   } catch (error) {
-//     console.error('Error during seeding:', error);
-//   }
-// };
+    console.log('Seeding complete: Admin user with admin role added.');
+  } catch (error) {
+    console.error('Error during seeding:', error);
+  }
+};
 
-// seedDatabase();
+seedDatabase();
